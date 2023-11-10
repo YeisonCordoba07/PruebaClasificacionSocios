e3bf24375670b83f9e837d7eeb941b0107f98be5
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-11-10 05:31:17 -0500

    Corrige pruebas unitarias a clase SocioController

--- src/test/java/ssmu/pruebaclasificacionsocios/controller/SocioControllerTest.java
+++ src/test/java/ssmu/pruebaclasificacionsocios/controller/SocioControllerTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoSession;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ssmu.pruebaclasificacionsocios.model.Socio;
 import ssmu.pruebaclasificacionsocios.service.SocioService;
+import org.mockito.MockitoSession;
+import org.mockito.quality.Strictness;
 
 import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
@@ -42,33 +45,33 @@ public class SocioControllerTest {
         socioEsperado.setNumeroDeAmonestaciones(3);
         socioEsperado.setNumeroDeFelicitaciones(1);
 
-        //Es el que se va a devolver
-        //Para que falle se hace diferente al de arriba (socioEsperado)
-        //Para que pase se hace igual
-        Socio socioDevuelto = new Socio();
-        socioDevuelto.setCedula(7000);
-        socioDevuelto.setNombre("No Juan 2");
-        socioDevuelto.setCiudad(12345);
-        socioDevuelto.setUsuario("nojuanperez123");
-        socioDevuelto.setClave("clave123");
-        socioDevuelto.setNumeroDeServicios(5);
-        socioDevuelto.setPromedioDeCalificacion(4.7F);
-        socioDevuelto.setNumeroDeAmonestaciones(3);
-        socioDevuelto.setNumeroDeFelicitaciones(1);
+            //Es el que se va a devolver
+            //Para que falle se hace diferente al de arriba (socioEsperado)
+            //Para que pase se hace igual
+            Socio socioDevuelto = new Socio();
+            socioDevuelto.setCedula(7000);
+            socioDevuelto.setNombre("No Juan 2");
+            socioDevuelto.setCiudad(12345);
+            socioDevuelto.setUsuario("nojuanperez123");
+            socioDevuelto.setClave("clave123");
+            socioDevuelto.setNumeroDeServicios(5);
+            socioDevuelto.setPromedioDeCalificacion(4.7F);
+            socioDevuelto.setNumeroDeAmonestaciones(3);
+            socioDevuelto.setNumeroDeFelicitaciones(1);
 
-        // Configura el comportamiento del servicio falso
-        // Cuando se diga findSocioByCedula(7000), va a devolver socioDevuelto
-        when(socioServiceMock.findSocioByCedula(7000)).thenReturn(socioDevuelto);
+            // Configura el comportamiento del servicio falso
+            // Cuando se diga findSocioByCedula(7000), va a devolver socioDevuelto
+            when(socioServiceMock.findSocioByCedula(7000)).thenReturn(socioDevuelto);
 
-        // Ejecuta el método que se está probando - devuelve socioEsparado
-        ResponseEntity<Socio> respuesta = socioController.findSocioByCedula(7000);
+            // Ejecuta el método que se está probando - devuelve socioEsparado
+            ResponseEntity<Socio> respuesta = socioController.findSocioByCedula(7000);
 
-        // Verifica el resultado
-        Assertions.assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+            // Verifica el resultado
+            Assertions.assertEquals(HttpStatus.OK, respuesta.getStatusCode());
 
 
-        Socio socioRespuesta = respuesta.getBody();
-        System.out.println("Socio respuesta" + respuesta.getBody());
+            Socio socioRespuesta = respuesta.getBody();
+            System.out.println("Socio respuesta" + respuesta.getBody());
 
         /*assertEquals(socioEsperado.getCedula(), socioRespuesta.getCedula());
         assertEquals(socioEsperado.getNombre(), socioRespuesta.getNombre());
@@ -80,10 +83,11 @@ public class SocioControllerTest {
         assertEquals(socioEsperado.getNumeroDeAmonestaciones(), socioRespuesta.getNumeroDeAmonestaciones());
         assertEquals(socioEsperado.getNumeroDeFelicitaciones(), socioRespuesta.getNumeroDeFelicitaciones());*/
 
-        // Verificar que los valores del objeto Socio simulado sean iguales a los del objeto devuelto por el controlador
-        Assertions.assertEquals(socioEsperado, socioRespuesta);
+            // Verificar que los valores del objeto Socio simulado sean iguales a los del objeto devuelto por el controlador
+            Assertions.assertEquals(socioEsperado, socioRespuesta);
 
-        // Verificar que el método del servicio se llamó con el parámetro correcto
-        verify(socioServiceMock, times(1)).findSocioByCedula(7000);
+            // Verificar que el método del servicio se llamó con el parámetro correcto
+            verify(socioServiceMock, times(1)).findSocioByCedula(7000);
+        
     }
 }
